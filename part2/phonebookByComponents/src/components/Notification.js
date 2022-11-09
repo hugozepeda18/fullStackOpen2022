0a1883617f2d4081010f668fd14c23a5792e78a4
hugo.zepeda <A01228679@exatec.tec.mx>
2022-11-09 15:36:21 -0600

    2.19-20*: Phonebook step 11-12

--- part2/phonebookByComponents/src/components/Notification.js
+++ part2/phonebookByComponents/src/components/Notification.js
@@ -1,24 +1,6 @@
 import React from 'react'
 
-const successStyle = {
-  color: 'green',
-  background: 'lightgrey',
-  font_size: 20,
-  border_style: 'solid',
-  border_radius: 5,
-  padding: 10,
-  margin_bottom: 10
-}
-
-const errorStyle = {
-  color: 'red',
-  background: 'lightgrey',
-  font_size: 20,
-  border_style: 'solid',
-  border_radius: 5,
-  padding: 10,
-  margin_bottom: 10
-}
+import { errorStyle, successStyle } from './Notification.style'
 
 const Notification = ({ message }) => {
     if (message === null) {
---
+++ part2/phonebookByComponents/src/components/Notification.style.js
@@ -0,0 +1,19 @@
+export const successStyle = {
+    color: 'green',
+    background: 'lightgrey',
+    font_size: 20,
+    border_style: 'solid',
+    border_radius: 5,
+    padding: 10,
+    margin_bottom: 10
+}
+  
+export const errorStyle = {
+    color: 'red',
+    background: 'lightgrey',
+    font_size: 20,
+    border_style: 'solid',
+    border_radius: 5,
+    padding: 10,
+    margin_bottom: 10
+}
