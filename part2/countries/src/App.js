3b9c6825fd35a52846a9e69b7e72cd6e252f1d28
hugo.zepeda <A01228679@exatec.tec.mx>
2022-11-23 17:09:54 -0600

    2.12* Data for countries, step1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,25 +4,29 @@ import axios from 'axios'
 function App() {
   
   //USE STATES
+  const [allCountries, setAllCountries] = useState([])
   const [countries, setCountries] = useState([])
-  const [newCountry, setNewCountry] = useState('')
   const [filterCountries, setFilterCountries] = useState([])
 
   //USE EFFECTS
   useEffect(() => {
+    console.log('I am starting the call')
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
         console.log('promise fulfilled')
         console.log(response.data)
-        setCountries(response.data)
+        setAllCountries(response.data)
       })
+      .catch(e => <h1>Please reload the page</h1>)
   }, [])
 
   //HANDLE FUNCTION
   const handleCountryChange = (event) => {
-    setNewCountry(event.target.value)
-    
+    setFilterCountries(event.target.value)
+    const regex = new RegExp( filterCountries, 'i')
+    const filteredCountries = () => allCountries.filter(country => country.name.common.match(regex))
+    setCountries(filteredCountries)
   }
 
   return (
@@ -31,12 +35,38 @@ function App() {
       <div>
         <h3>Find countries:</h3>
         <input 
-          value={newCountry}
+          value={filterCountries}
           onChange={handleCountryChange}
           />
       </div>
       <div>
         <h2>Matched countries:</h2>
+        { (countries.length === 0) ? <div></div> :
+            (countries.length > 10) ? 
+            <p>Too many matches, specify another filter</p>
+            : (countries.length > 1) ? 
+              <ul>
+                {countries.map(country => <li>{country.name.common}</li>)}
+              </ul> :
+              <div>
+                {countries.map(country => {
+                  return(
+                    <>
+                      <h1>{country.name.common}</h1>
+                      <p>capital:</p>
+                      <p>{country.capital}</p>
+                      <p>area:</p>
+                      <p>{country.area}</p>
+                      <p>Languages:</p>
+                      <ul>
+                        {Object.values(country.languages)}
+                      </ul>
+                      <img src={country.flags.png} alt="Country Flag"/>
+                    </>
+                  )
+                })}
+              </div>
+        }
       </div>
     </>
   );
