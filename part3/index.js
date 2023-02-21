76c0b249092f166318d2215c349a8ac29a1d1c65
hugo.zepeda <A01228679@exatec.tec.mx>
2023-02-21 11:39:49 -0600

    3.2: Phonebook backend step2

--- part3/index.js
+++ part3/index.js
@@ -30,6 +30,14 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const request = new Date()
+    res.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        ${request.toJSON()}`
+    )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
