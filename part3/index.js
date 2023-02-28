bc84123909f4115e71aea16cea691643f02b076b
hugo.zepeda <A01228679@exatec.tec.mx>
2023-02-28 13:23:18 -0600

    3.9 phonebook backend step9

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res[content]', undefined))
+app.use(cors())
 
 let persons = [
     {
@@ -90,7 +92,7 @@ app.get('/info', (req, res) => {
     )
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
