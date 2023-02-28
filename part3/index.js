4a309f65a3b92bbc3f1ab380a2aafc81d648d4ec
hugo.zepeda <A01228679@exatec.tec.mx>
2023-02-28 13:27:27 -0600

    3.10 phonebook backend step10

--- part3/index.js
+++ part3/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res[content]', undefined))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
